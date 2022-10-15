14b958ae2a99cb013d39cf13a18bc59ee9134bab
Joseph Maa <josephgmaa@berkeley.edu>
2022-10-14 22:13:44 -0700

    Modify default ports, remove js file in contracts folder, update package-lock.json

--- truffle-config.js
+++ truffle-config.js
@@ -43,9 +43,9 @@ module.exports = {
     //
      development: {
       host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "1337",       // Any network (default: none)
-     },
+      port: 8546,            // Standard Ethereum port (default: none)
+      network_id: 5777,       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -71,6 +71,10 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    rpc: {
+      host: "127.0.0.1",
+      port: 8545
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
